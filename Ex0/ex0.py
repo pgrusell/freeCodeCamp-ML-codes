8abdccc8bceb56006fef314ef0bc346b123320e3
pgrusell <pgrusell@MacBook-Pro-de-Pablo.local>
2025-04-02 10:48:04 +0200

    Slight modification on the input data set, introducing some Gaussian noise.

--- Ex0/ex0.py
+++ Ex0/ex0.py
@@ -4,25 +4,33 @@ import numpy.linalg as npl
 import matplotlib.pyplot as plt
 from utils import NN
 
+# Plot style
+plt.rcParams['font.size'] = 10
+plt.rcParams['text.usetex'] = True
+plt.rcParams['font.family'] = 'serif'
+
 # Generate some random data
-x_data = npr.uniform(-10, 10, (1000, 2))
-y_data = npl.norm(x_data, axis=1, keepdims=True)
+n_samples = 10000
+
+x_data = npr.uniform(-10, 10, (n_samples, 2))
+y_data = npl.norm(x_data, axis=1, keepdims=True) + \
+    npr.normal(0, 1, (n_samples, 1))  # norm + Gaussian noise
 
 # Build the NN
 neural_network = NN(n_layers=4, n_neurons=32,
                     x_data=x_data, y_data=y_data)
 neural_network.build()
-neural_network.train(obj=0.01, eta=0.00001)
+neural_network.train(obj=0.01, eta=0.000001)
 
 # Testing
-x_test = npr.uniform(-10, 10, (200, 2))
+x_test = npr.uniform(-10, 10, (400, 2))
 y_true = npl.norm(x_test, axis=1, keepdims=True)
 
 # Prediction
 y_pred = np.array([neural_network.predict(x) for x in x_test])
-y_pred = y_pred.reshape(-1, 1)
+# y_pred = y_pred.reshape(-1, 1)
 
-# Cálculo error
+# Error
 mse = np.mean((y_pred - y_true) ** 2)
 print(f"Test MSE: {mse:.6f}")
 
@@ -44,7 +52,7 @@ plt.show()
 error = y_pred - y_true
 
 plt.figure(figsize=(8, 6))
-plt.hist(y_pred - y_true, bins=np.linspace(error.min(), error.max(),
+plt.hist(error, bins=np.linspace(error.min(), error.max(),
          20), color='blue', edgecolor='black', alpha=0.4, label=f'mean = {error.mean() : .3f}\nstd = {error.std() : .3f}')
 plt.title("Error distribution")
 plt.xlabel("Error")
